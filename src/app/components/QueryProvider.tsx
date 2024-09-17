664cd834d1ccb6bc0354c0e61ad7f5e6f403a6fd
Lucas Lobell <lucaslobell2@gmail.com>
2024-09-17 14:09:12 -0300

    test

--- src/app/components/QueryProvider.tsx
+++ src/app/components/QueryProvider.tsx
@@ -2,16 +2,16 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 type Props = {
-    children:React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
 const QueryProvider = ({ children }: Props) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
-}
+  );
+};
 
-export default QueryProvider
+export default QueryProvider;
