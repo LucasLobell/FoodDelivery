5c5db77e73c25f6eecc95f1439a76f2ef9ebc72c
Lucas Lobell <lucaslobell2@gmail.com>
2024-09-17 16:54:42 -0300

    cart and lots of math

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,60 +1,66 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+
+import { useCartStore } from "@/utils/store";
+import Image from "next/image";
+import React, { useEffect } from "react";
 
 const CartPage = () => {
+  const { products, totalItems, totalPrice, removeFromCart } = useCartStore();
+
+  useEffect(() =>{
+    useCartStore.persist.rehydrate()
+  },[])
+
   return (
-    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row'>
-      <div className='h-1/2 p-4 flex flex-col justify-center overflow-auto lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6' style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-        <div className='flex items-center justify-between mb-4'>
-          <Image src="/temporary/p1.png" alt='' width={100} height={100} />
-          <div>
-            <h1 className='uppercase text-xl font-bold'>Sicilian</h1>
-            <span>Large</span>
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
+      <div
+        className="h-1/2 p-4 flex flex-col justify-center overflow-auto lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6"
+        style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+      >
+        {products.map((item) => (
+          <div className="flex items-center justify-between mb-4" key={item.id}>
+            {item.img && (
+              <Image src={item.img} alt="" width={100} height={100} />
+            )}
+            <div>
+              <h1 className="uppercase text-xl font-bold">{item.title} x{item.quantity}</h1>
+              <span>{item.optionTitle}</span>
+            </div>
+            <h2 className="font-bold">R${item.price}</h2>
+            <span
+              className="cursor-pointer"
+              onClick={() => removeFromCart(item)}
+            >
+              X
+            </span>
           </div>
-          <h2 className='font-bold cursor-pointer'>R$79.90</h2>
-          <span>X</span>
-        </div>
-        <div className='flex items-center justify-between mb-4'>
-          <Image src="/temporary/p1.png" alt='' width={100} height={100} />
-          <div>
-            <h1 className='uppercase text-xl font-bold'>Sicilian</h1>
-            <span>Large</span>
-          </div>
-          <h2 className='font-bold cursor-pointer'>R$79.90</h2>
-          <span>X</span>
-        </div>
-        <div className='flex items-center justify-between mb-4'>
-          <Image src="/temporary/p1.png" alt='' width={100} height={100} />
-          <div>
-            <h1 className='uppercase text-xl font-bold'>Sicilian</h1>
-            <span>Large</span>
-          </div>
-          <h2 className='font-bold cursor-pointer'>R$79.90</h2>
-          <span>X</span>
-        </div>
+        ))}
       </div>
-      <div className='h-1/2 p-4 bg-fuchsia-50 gap-4 flex flex-col justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6'>
-        <div className='flex justify-between'>
-          <span>Subtotal (3 items)</span>
-          <span>R$81.70</span>
+
+      <div className="h-1/2 p-4 bg-fuchsia-50 gap-4 flex flex-col justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+        <div className="flex justify-between">
+          <span>Subtotal ({totalItems} items)</span>
+          <span>R${totalPrice}</span>
         </div>
-        <div className='flex justify-between'>
+        <div className="flex justify-between">
           <span>Service Cost</span>
           <span>R$0.00</span>
         </div>
-        <div className='flex justify-between'>
+        <div className="flex justify-between">
           <span>Delivery Cost</span>
-          <span className='text-green-500'>FREE!</span>
+          <span className="text-green-500">FREE!</span>
         </div>
-        <hr className='my-2' />
-        <div className='flex justify-between'>
+        <hr className="my-2" />
+        <div className="flex justify-between">
           <span>Total</span>
-          <span className='font-bold'>R$81.70</span>
+          <span className="font-bold">R${totalPrice}</span>
         </div>
-        <button className='bg-red-500 text-white p-4 rounded-md w-1/2 self-end'>Checkout</button>
+        <button className="bg-red-500 text-white p-4 rounded-md w-1/2 self-end">
+          Checkout
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartPage
+export default CartPage;
